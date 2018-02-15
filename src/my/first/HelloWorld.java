5b9d2898ceb76abd64d6a38cd2a11bb9d58acb16
Szakos Patrik <szakos2@lab103-07.iit.uni-miskolc.hu>
2018-02-15 12:42:39 +0100

    HelloWorld

---
+++ src/my/first/HelloWorld.java
@@ -0,0 +1,14 @@
+package my.first;
+
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String message = "Hello world";
+		System.out.println(message);
+
+	}
+
+}
